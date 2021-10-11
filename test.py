1d5ad1e9c75ad42ad6625d91a01043112465a327
武井美優 <camt1200380@gn.iwasaki.ac.jp>
2021-10-12 00:31:13 +0900

    fileの読み込み書き込み

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-import sys
-import fileinput
-from pathlib import Path
-
-#引数をファイル名かstdinから読み込む
-if Path(sys.argv[1]).exists():#第一引数がファイルだったら
-    print('file input')#ファイルの内容を1行ずつprint
-    print(line)
-else:#第一引数がファイルではなかったら
-    for i in sys.argv[1:]:
-        print('args input')#引数の文字列をprint
-        print(i)
+def test(file1):
+    f=open(file1,'r',encofing='UTF-8')
+    data=f.read()
+    print(type(data))
+    f.close()
